0ee842a520888ae8df665df5e4df37bc135d7698
Brian Richwine <brichwin@iu.edu>
2025-01-01 18:35:42 -0500

    player rotation with a and d keys

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 from circleshape import *
 
 
@@ -18,5 +18,16 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
     def draw(self, screen):
         pygame.draw.polygon(screen,(255,255,255), self.triangle(), 2)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
